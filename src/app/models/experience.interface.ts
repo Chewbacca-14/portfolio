1cb74a33044cc5a7e4f71e246c885637938d68bd
USpiri <urielspiridione135@gmail.com>
2024-01-04 20:04:18 -0300

    ✨ Feat: Experiences mock and interface

---
+++ src/app/models/experience.interface.ts
@@ -0,0 +1,8 @@
+export interface Experience {
+  id: string;
+  date: string;
+  company: string;
+  job: string;
+  url?: string;
+  description: string;
+}
--- src/app/pages/home/components/experience/experience.component.ts
+++ src/app/pages/home/components/experience/experience.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { EXPERIENCES } from '@assets/data/experience.mock';
 import { LinkIconComponent } from '@shared/components/link-icon/link-icon.component';
+import { Experience } from 'src/app/models/experience.interface';
 
 @Component({
   selector: 'app-experience',
@@ -9,75 +11,44 @@ import { LinkIconComponent } from '@shared/components/link-icon/link-icon.compon
     <section>
       <h2 class="text-4xl font-semibold">Experience</h2>
       <ul class="mt-10 flex flex-col gap-3">
-        <li
-          class="flex group/item hover:scale-[102%] transition-all flex-col md:flex-row md:gap-6 md:items-center border-neutral-800 rounded-lg py-3"
-        >
-          <div
-            class="shrink-0 w-32 group-hover/item:text-cyan-500 transition-all text-neutral-400 font-medium md:text-right"
+        @for (experience of experiences; track experience.id) {
+          <li
+            class="flex group/item hover:scale-[102%] transition-all flex-col md:flex-row md:gap-6 md:items-center border-neutral-800 rounded-lg py-3"
           >
-            2022 - 2023
-          </div>
-          <div class="flex flex-col gap-1">
-            <h3 class="text-2xl font-semibold flex items-center gap-2">
-              <a href="/" class="hover:underline">Jojo Mobile</a>
-              <app-link-icon class="h-4 w-4 fill-neutral-50" />
-            </h3>
-            <p class="text-neutral-300 text-sm font-medium">
-              Android Developer → Lead Android Developer
-            </p>
-            <p class="text-neutral-400">
-              Built an Android team, created Android apps for biggest Polish
-              companies like LOT, Polskie Radio, Agora, PolskaPress
-            </p>
-          </div>
-        </li>
-        <li
-          class="flex group/item hover:scale-[102%] transition-all flex-col md:flex-row md:gap-6 md:items-center border-neutral-800 rounded-lg py-3"
-        >
-          <div
-            class="shrink-0 w-32 group-hover/item:text-cyan-500 transition-all text-neutral-400 font-medium md:text-right"
-          >
-            2022 - 2023
-          </div>
-          <div class="flex flex-col gap-1">
-            <h3 class="text-2xl font-semibold flex items-center gap-2">
-              <a href="/" class="hover:underline">Jojo Mobile</a>
-              <app-link-icon class="h-4 w-4 fill-neutral-50" />
-            </h3>
-            <p class="text-neutral-300 text-sm font-medium">
-              Android Developer → Lead Android Developer
-            </p>
-            <p class="text-neutral-400">
-              Built an Android team, created Android apps for biggest Polish
-              companies like LOT, Polskie Radio, Agora, PolskaPress
-            </p>
-          </div>
-        </li>
-        <li
-          class="flex group/item hover:scale-[102%] transition-all flex-col md:flex-row md:gap-6 md:items-center border-neutral-800 rounded-lg py-3"
-        >
-          <div
-            class="shrink-0 w-32 group-hover/item:text-cyan-500 transition-all text-neutral-400 font-medium md:text-right"
-          >
-            2022 - 2023
-          </div>
-          <div class="flex flex-col gap-1">
-            <h3 class="text-2xl font-semibold flex items-center gap-2">
-              <a href="/" class="hover:underline">Jojo Mobile</a>
-              <!-- <app-link-icon class="h-4 w-4 fill-neutral-50" /> -->
-            </h3>
-            <p class="text-neutral-300 text-sm font-medium">
-              Android Developer → Lead Android Developer
-            </p>
-            <p class="text-neutral-400">
-              Built an Android team, created Android apps for biggest Polish
-              companies like LOT, Polskie Radio, Agora, PolskaPress
-            </p>
-          </div>
-        </li>
+            <div
+              class="shrink-0 w-32 group-hover/item:text-cyan-500 transition-all text-neutral-400 font-medium md:text-right"
+            >
+              {{ experience.date }}
+            </div>
+            <div class="flex flex-col gap-1">
+              <h3 class="text-2xl font-semibold flex items-center gap-2">
+                @if (experience.url) {
+                  <a
+                    [href]="experience.url"
+                    class="hover:underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >{{ experience.company }}</a
+                  >
+                  <app-link-icon class="h-4 w-4 fill-neutral-50" />
+                } @else {
+                  {{ experience.company }}
+                }
+              </h3>
+              <p class="text-neutral-300 text-sm font-medium">
+                {{ experience.job }}
+              </p>
+              <p class="text-neutral-400">
+                {{ experience.description }}
+              </p>
+            </div>
+          </li>
+        }
       </ul>
     </section>
   `,
   styles: ``,
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+  experiences: Experience[] = EXPERIENCES;
+}

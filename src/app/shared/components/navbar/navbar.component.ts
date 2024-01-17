b096e15b9d6bcc6b5f8d4f6994637a8e4f88941d
USpiri <urielspiridione135@gmail.com>
2024-01-16 21:23:20 -0300

    ✨ Feat: Add home routing to navbar logo

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DevlyIconComponent } from '../devly-icon/devly-icon.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [DevlyIconComponent],
+  imports: [DevlyIconComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styles: ``,
 })

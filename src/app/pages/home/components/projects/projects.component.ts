3213aae3aa8e5e831c57554b848cb5f8ed249dc9
USpiri <urielspiridione135@gmail.com>
2024-01-17 01:26:05 -0300

    🐛 Fix: Projects tags constrast and sizes

--- src/app/pages/home/components/projects/projects.component.ts
+++ src/app/pages/home/components/projects/projects.component.ts
@@ -16,44 +16,47 @@ import { Project } from 'src/app/models/project.interface';
         @for (project of projects; track project.id) {
           <li>
             <article
-              class="px-5 py-4 border border-neutral-800 rounded-lg hover:scale-[102%] transition-all"
+              class="rounded-lg border border-neutral-800 px-5 py-4 transition-all hover:scale-[102%]"
             >
-              <header class="flex flex-row gap-3 items-center mb-2 flex-wrap">
-                <h3 class="text-xl font-semibold text-nowrap">
+              <header class="mb-3 flex flex-row flex-wrap items-center gap-3">
+                <h3 class="text-nowrap text-xl font-semibold md:text-2xl">
                   {{ project.title }}
                 </h3>
                 <ul class="flex gap-3">
                   @for (tag of project.tags; track $index) {
                     <li
-                      class="px-1 border rounded-lg text-sm border-neutral-700 text-neutral-500 font-semibold"
+                      class="rounded-lg border border-neutral-600 px-1 text-sm text-neutral-400"
                     >
                       {{ tag }}
                     </li>
                   }
                 </ul>
               </header>
-              <p class="text-sm text-neutral-400 mb-3">
+              <p class="mb-4 leading-snug text-neutral-400">
                 {{ project.description }}
               </p>
               <footer class="flex gap-2">
                 <a
                   [href]="project.url"
-                  class="hover:scale-110 transition-all fill-neutral-50 border border-neutral-600 rounded-lg p-2 text-sm"
+                  class="rounded-lg border border-neutral-600 fill-neutral-50 p-2 text-sm transition-all hover:scale-110"
                   target="_blank"
                   rel="noopener noreferrer"
+                  title="Project Link"
                 >
                   <app-link-icon class="h-4 w-4" />
                 </a>
                 <a
                   [href]="project.repo"
-                  class="hover:scale-110 transition-all fill-neutral-50 border border-neutral-600 rounded-lg p-2 text-sm"
+                  class="rounded-lg border border-neutral-600 fill-neutral-50 p-2 text-sm transition-all hover:scale-110"
                   target="_blank"
                   rel="noopener noreferrer"
+                  title="Project repo"
                 >
                   <app-github-icon class="h-4 w-4" />
                 </a>
                 <button
-                  class="hover:scale-110 transition-all fill-neutral-50 border border-neutral-600 rounded-lg p-2 text-sm"
+                  class="rounded-lg border border-neutral-600 fill-neutral-50 p-2 text-sm transition-all hover:scale-110"
+                  title="Project info"
                 >
                   <app-info-icon class="h-4 w-4" />
                 </button>

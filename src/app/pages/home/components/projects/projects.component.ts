4dac5114561e51bfb3ece767195b5ce1e67c748b
USpiri <urielspiridione135@gmail.com>
2024-01-17 21:37:42 -0300

    🛑 Remove: Unused imports

--- src/app/pages/home/components/projects/projects.component.ts
+++ src/app/pages/home/components/projects/projects.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 import { PROJECTS } from '@assets/data/projects.mock';
-import { ButtonComponent } from '@shared/components/button/button.component';
-import { GithubIconComponent } from '@shared/components/github-icon/github-icon.component';
-import { InfoIconComponent } from '@shared/components/info-icon/info-icon.component';
-import { LinkButtonComponent } from '@shared/components/link-button/link-button.component';
-import { LinkIconComponent } from '@shared/components/link-icon/link-icon.component';
 import { Project } from 'src/app/models/project.interface';
 import { ProjectComponent } from '../project/project.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [
-    LinkIconComponent,
-    GithubIconComponent,
-    InfoIconComponent,
-    LinkButtonComponent,
-    ButtonComponent,
-    ProjectComponent,
-  ],
+  imports: [ProjectComponent],
   template: `
     <section id="projects">
       <h2 class="text-4xl font-semibold">Projects</h2>

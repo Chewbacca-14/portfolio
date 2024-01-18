440b58ebe877d4335740309d35d3c47c660d91e2
USpiri <urielspiridione135@gmail.com>
2024-01-17 21:31:49 -0300

    ✨ Feat: Replace project component

--- src/app/pages/home/components/projects/projects.component.ts
+++ src/app/pages/home/components/projects/projects.component.ts
@@ -6,6 +6,7 @@ import { InfoIconComponent } from '@shared/components/info-icon/info-icon.compon
 import { LinkButtonComponent } from '@shared/components/link-button/link-button.component';
 import { LinkIconComponent } from '@shared/components/link-icon/link-icon.component';
 import { Project } from 'src/app/models/project.interface';
+import { ProjectComponent } from '../project/project.component';
 
 @Component({
   selector: 'app-projects',
@@ -16,6 +17,7 @@ import { Project } from 'src/app/models/project.interface';
     InfoIconComponent,
     LinkButtonComponent,
     ButtonComponent,
+    ProjectComponent,
   ],
   template: `
     <section id="projects">
@@ -23,51 +25,7 @@ import { Project } from 'src/app/models/project.interface';
       <ul class="mt-10 flex flex-col gap-3">
         @for (project of projects; track project.id) {
           <li>
-            <article
-              class="rounded-lg border border-neutral-800 px-5 py-4 transition-all hover:scale-[102%]"
-            >
-              <header class="mb-3 flex flex-row flex-wrap items-center gap-3">
-                <h3 class="text-nowrap text-xl font-semibold md:text-2xl">
-                  {{ project.title }}
-                </h3>
-                <ul class="flex gap-3">
-                  @for (tag of project.tags; track $index) {
-                    <li
-                      class="rounded-lg border border-neutral-600 px-1 text-sm text-neutral-400"
-                    >
-                      {{ tag }}
-                    </li>
-                  }
-                </ul>
-              </header>
-              <p class="mb-4 leading-snug text-neutral-400">
-                {{ project.description }}
-              </p>
-              <footer class="flex gap-2">
-                @if (project.url) {
-                  <app-link-button
-                    [href]="project.url"
-                    title="{{ project.title }} page"
-                  >
-                    <app-link-icon class="h-4 w-4" />
-                  </app-link-button>
-                }
-                @if (project.repo) {
-                  <app-link-button
-                    [href]="project.repo"
-                    title="{{ project.title }} repository"
-                  >
-                    <app-github-icon class="h-4 w-4" />
-                  </app-link-button>
-                }
-                <app-button icon>
-                  <app-info-icon
-                    class="h-4 w-4"
-                    title="{{ project.title }} more info"
-                  />
-                </app-button>
-              </footer>
-            </article>
+            <app-project [project]="project" />
           </li>
         }
       </ul>

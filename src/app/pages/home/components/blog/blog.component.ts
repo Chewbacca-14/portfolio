26594ba7536cb617e58fc24fcfd0fb7ab4079570
USpiri <urielspiridione135@gmail.com>
2024-01-17 21:43:48 -0300

    ✨ Feat: Replace post component and remove unnecesary imports

--- src/app/pages/home/components/blog/blog.component.ts
+++ src/app/pages/home/components/blog/blog.component.ts
@@ -1,13 +1,12 @@
-import { DatePipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { Post } from '@models';
-import { LinkIconComponent } from '@shared/components/link-icon/link-icon.component';
 import { BlogService } from '@shared/services/blog.service';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [LinkIconComponent, DatePipe],
+  imports: [PostComponent],
   template: `
     <section id="blog">
       <h2 class="text-4xl font-semibold">Blog</h2>
@@ -16,35 +15,7 @@ import { BlogService } from '@shared/services/blog.service';
       >
         @for (post of posts; track post.id) {
           <li class="flex-[1_0_40%]">
-            <a
-              [href]="post.url"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex h-fit flex-col rounded-lg px-4 py-3 transition-all hover:scale-[102%] hover:bg-neutral-500/10"
-            >
-              <header>
-                <span class="text-sm font-semibold text-neutral-400/80">{{
-                  post.date | date
-                }}</span>
-                <h3 class="mb-1 line-clamp-2 text-xl font-semibold md:text-2xl">
-                  {{ post.title }}
-                </h3>
-              </header>
-              <p
-                class="mt-1.5 line-clamp-2 text-sm font-medium text-neutral-500 md:text-base dark:text-neutral-400/80"
-              >
-                {{ post.description }}
-              </p>
-              <footer class="mt-1.5 flex flex-row items-center gap-2">
-                @for (tag of post.tags; track $index) {
-                  <div
-                    class="text-sm font-semibold text-neutral-600 md:text-base dark:text-neutral-300"
-                  >
-                    #{{ tag }}
-                  </div>
-                }
-              </footer>
-            </a>
+            <app-post [post]="post" />
           </li>
         }
       </ul>

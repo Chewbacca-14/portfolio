3cebca33da7e27d91e87d673f47db64d52563d72
USpiri <urielspiridione135@gmail.com>
2024-02-05 23:57:11 -0300

    ✨ Feat: Restyle navbar and add cv

--- src/app/shared/components/link-button/link-button.component.ts
+++ src/app/shared/components/link-button/link-button.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from '@angular/core';
   template: `
     <a
       class="group block rounded-md {{
-        btnStyle === 'outline' && 'border'
-      }} border-neutral-500 fill-neutral-50 p-2 transition-all hover:scale-105 hover:bg-neutral-400/10 active:scale-90"
+        btnStyle === 'outline' ? 'border p-2' : 'py-1 px-2'
+      }} border-neutral-500 fill-neutral-50 transition-all hover:scale-105 hover:bg-neutral-400/10 active:scale-90"
       [href]="href"
       target="_blank"
       rel="noopener noreferrer"

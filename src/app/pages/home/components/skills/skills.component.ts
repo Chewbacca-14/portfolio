032476004289d606c59cd72f19b1b0594f58e07d
USpiri <urielspiridione135@gmail.com>
2024-02-05 23:35:06 -0300

    🎨 Style: Reestyle skill tag

--- src/app/pages/home/components/skills/skills.component.ts
+++ src/app/pages/home/components/skills/skills.component.ts
@@ -12,7 +12,7 @@ import { SKILLS } from '@shared/data/skills.mock';
       <ul class="mt-10 flex flex-wrap justify-center gap-3">
         @for (skill of skills; track $index) {
           <li
-            class="rounded-lg border border-neutral-600 px-2 py-1 text-base font-semibold text-neutral-400 transition-all hover:scale-110 hover:text-cyan-500"
+            class="rounded-md border border-neutral-600 px-2 py-1 text-base font-semibold text-neutral-400 transition-all hover:scale-105 hover:text-cyan-500"
           >
             {{ skill }}
           </li>

8aa9e71562bcf2bc3e1618685a4fa1fce67cdaab
USpiri <urielspiridione135@gmail.com>
2024-02-05 23:37:31 -0300

    ✨ Feat: Change component order

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -19,8 +19,8 @@ import { BlogComponent } from './components/blog/blog.component';
     <div class="flex justify-center py-16 print:py-0 md:py-28">
       <main class="mx-4 flex w-full max-w-3xl flex-col gap-20 md:mx-8">
         <app-header />
-        <app-projects />
         <app-experience />
+        <app-projects />
         <app-skills />
         <app-blog />
       </main>

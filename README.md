ec5ab09e46efd0e8e819524914db4663f24e08e0
Maxim <git config credential.helpe>
2024-12-16 09:52:53 +0100

    build

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio2024
-Fork of https://github.com/USpiri/uspiri.com
--- src/app/models/project.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Project {
-  id: number;
-  name: string;
-  tags: string[];
-  url: string | null;
-  repo: string | null;
-  description: string;
-}
